3c9eaf6ccc7c528b33d8edc26d64b37908d16cfe
Mathew Brown <matt@mattbrown.com>
2017-06-07 10:54:51 -0600

    Add cookie-parser to app.

--- server.js
+++ server.js
@@ -2,11 +2,12 @@ const express = require('express');
 const app = express();
 
 const bodyParser = require('body-parser');
-
+const cookieParser = require('cookie-parser');
 const token = require('./routes/token');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 app.use('/token', token);
 
