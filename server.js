f23325d980316aa64f00e8a83e1d0793797af0db
Mathew Brown <matt@mattbrown.com>
2017-06-07 10:53:03 -0600

    Add token route and use it in app.

--- server.js
+++ server.js
@@ -1,4 +1,13 @@
 const express = require('express');
 const app = express();
 
+const bodyParser = require('body-parser');
+
+const token = require('./routes/token');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use('/token', token);
+
 app.listen('3000');
